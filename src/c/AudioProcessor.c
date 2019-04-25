ec18442a1f446ec4ae04dcd854edec8493c47e6f
Geo25rey <gcasper42@gmail.com>
2019-04-24 20:01:45 -0400

    Added functionality to AudioProcessor

--- src/c/AudioProcessor.c
+++ src/c/AudioProcessor.c
@@ -1,2 +1,31 @@
-// AudioProcessor.c 
-// Backend for FFT wave analysis
+#include <opus/opus.h>
+
+#include <AudioProcessor.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+static OpusDecoder* decoder = NULL;
+
+#define CHANNELS 2
+#define BITRATE 48000
+#define DECODE_FORWARD_ERROR_CORRECTION false
+
+static void init() {
+    int error = OPUS_OK;
+    do {
+        decoder = opus_decoder_create (BITRATE, CHANNELS, &error);
+    } while (error != OPUS_OK);
+}
+
+float* process_audio(uint8_t* unprocessed_data, int size, int frame_size) {
+    if (decoder == NULL)
+        init();
+    float* result = malloc(CHANNELS * sizeof(float) * size);
+    int error = opus_decode_float(decoder, unprocessed_data, size, result, frame_size, DECODE_FORWARD_ERROR_CORRECTION);
+    if (error != OPUS_OK) {
+        free(result);
+        return NULL;
+    }
+    return result;
+}
---
+++ src/c/include/AudioProcessor.h
@@ -0,0 +1,6 @@
+#ifndef AUDIO_PROCESSOR_H
+#define AUDIO_PROCESSOR_H
+
+float* process_audio(uint8_t* unprocessed_data, int size, int frame_size);
+
+#endif
